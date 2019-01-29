0ac86fd7e845aa7c3f2c557786a7e78e671644a4
Kathy Chen <kathychen.social@gmail.com>
2019-01-28 22:35:51 -0500

    uploading changes

--- assets/javascript/word-guess-game.js
+++ assets/javascript/word-guess-game.js
@@ -1,7 +1,32 @@
-/// VARIABLES
+$(document).ready(function () { 
+    
+});
 
+/// GLOBAL VARIABLES
+var wordBlank=["_ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _"]
+var wordAnswers=["festivus","spongeworthy","regifter","manhands","shrinkage","rageaholic"]
+var currentWord;
+var guessLetter;
+var wrongAnswerCount; 
+var numberOfGuesses;
 
 /// FUNCTIONS
+
+//1. add sections that makes up the game screen
+
+//2. select random variable length of wordBank
+
+//3. listen to command from user when keys are pressed
+
+//4. check answers to see if the letter is correct 
+
+//5. check answer to see if the entire word has been solved
+
+//6. otherwise wrong answers means a letter was not found in the word
+
+//7. otherwise a wrong answer will reduce the number of guesses by 1
+
+//8. win or lose message displayed
 
 
 /// EXECUTE GAME
